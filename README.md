74b2dcd513f81c5aa9b231e57ba013f7371d54ca
NookieNoo <biv0208@yandex.ru>
2021-04-02 22:54:47 +0300

    Завели подключение к бд

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # node-react
-node-react
+
+
+----
+
+#### Локальная разработка
+Подтягиваем зависимости
+> npm install
+
+Запуск для разработки
+> npm run start
+
+Запускаем сервер
+> npm run server
+
+###TODO
+* ??
---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const PORT = config.get('port') || 5000;
+
+const app = express();
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,   // TODO Узнать, что за параметры
+            useCreateIndex: true
+        });
+        app.listen(5000, () => console.log(`App started on port ${PORT}`));
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+start();
