5af4958dd03d15fcf9452e3eda322e09a08ce1f7
NookieNoo <biv0208@yandex.ru>
2021-04-05 15:54:32 +0300

    Добавляем еще что-то

---
+++ routes/link.routes.js
@@ -0,0 +1,15 @@
+const {Router} = require('express');
+const Link = require('../models/Link');
+const router = Router();
+
+router.post('/generate', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+router.post('/:id', async (req, res) => {
+
+});
